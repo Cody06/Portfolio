b7a82dd903e84aa1447adf1ad8f43931516edb30
Cody06 <codydev22@gmail.com>
2023-09-27 16:15:44 -0400

    Configure tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,21 @@ const config: Config = {
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
+        colors: {
+            transparent: 'transparent',
+            // TODO: higher number should be a darker color
+            grey: {
+                100: '#353A40',
+                80: '#6C757D',
+                20: '#F0F0F0',
+            },
+            orange: {
+                100: '#F2AA4C',
+                80: '#774709',
+            },
+            black: '#000000',
+            white: '#FFFFFF',
+        },
         extend: {},
     },
     plugins: [],
