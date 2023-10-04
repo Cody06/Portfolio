f1982f437b450d27112429ee66dd8152daec5200
Cody06 <codydev22@gmail.com>
2023-10-04 16:38:44 -0400

    Move types to types.ts

---
+++ src/components/Workboard/types.ts
@@ -0,0 +1,16 @@
+export type CardData = {
+    id: string;
+    text: string;
+};
+
+export type ColumnData = {
+    id: string;
+    title: string;
+    cards: CardData[];
+};
+
+export type ItemToDelete = {
+    id: string;
+    kind: 'card' | 'column';
+    text: string;
+};
