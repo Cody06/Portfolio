9a807a2e42e89c40caf6a4a73636e1a87aa2951d
Cody06 <codydev22@gmail.com>
2023-10-05 16:00:27 -0400

    Add Experience & Certs to LandingPage

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -3,13 +3,17 @@ import TopNav from './components/TopNav';
 import Header from './components/Header';
 import ProjectsCollection from './components/ProjectsCollection';
 import Footer from './components/Footer';
+import WorkExperience from './components/WorkExperience';
+import Certifications from './components/Certifications';
 
 const LandingPage = () => (
     <>
         <TopNav />
-        <div className="content-max-width m-auto px-4 pt-24">
+        <div className="content-max-width m-auto px-4 pt-24 select-none">
             <Header />
             <ProjectsCollection />
+            <WorkExperience />
+            <Certifications />
             <Footer />
         </div>
     </>
