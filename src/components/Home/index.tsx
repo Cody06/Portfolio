24fec5a11324179c413b4cf6a8c60821d5d6cd9d
Cody06 <codydev22@gmail.com>
2023-10-07 18:19:57 -0400

    Change body background through useEffect

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -5,18 +5,25 @@ import ProjectsCollection from './components/ProjectsCollection';
 import Footer from './components/Footer';
 import WorkExperience from './components/WorkExperience';
 import Certifications from './components/Certifications';
+import { useEffect } from 'react';
 
-const Home = () => (
-    <>
-        <TopNav />
-        <div className="content-max-width m-auto px-4 pt-24 select-none">
-            <Header />
-            <ProjectsCollection />
-            <WorkExperience />
-            <Certifications />
-            <Footer />
-        </div>
-    </>
-);
+const Home = () => {
+    useEffect(() => {
+        document.body.className = '';
+    }, []);
+
+    return (
+        <>
+            <TopNav />
+            <div className="content-max-width m-auto px-4 pt-24 select-none">
+                <Header />
+                <ProjectsCollection />
+                <WorkExperience />
+                <Certifications />
+                <Footer />
+            </div>
+        </>
+    );
+};
 
 export default Home;
