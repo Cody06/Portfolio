e1b345bdabbbec51286d2439a13d65b686a43ca5
Cody06 <codydev22@gmail.com>
2023-10-09 14:01:51 -0400

    Update Footer style

--- src/components/Home/components/Footer.tsx
+++ src/components/Home/components/Footer.tsx
@@ -1,3 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLocationPin } from '@fortawesome/free-solid-svg-icons';
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
 import Link from 'next/link';
 
 const Footer = () => {
@@ -5,34 +11,62 @@ const Footer = () => {
         {
             href: 'mailto:cody.miu@gmail.com',
             title: 'Send me an email!',
-            label: 'cody.miu@gmail.com',
+            label: (
+                <>
+                    <FontAwesomeIcon icon={faEnvelope} className="fa-lg" />
+                    <span>cody.miu@gmail.com</span>
+                </>
+            ),
         },
         {
-            href: 'http://www.linkedin.com/in/cody-miu',
-            title: 'Visit my linkedin page!',
-            label: 'LinkedIn',
+            href: 'https://github.com/Cody06',
+            title: 'Visit my GitHub!',
+            label: (
+                <>
+                    <FontAwesomeIcon icon={faSquareGithub} className="fa-lg" />
+                    <span>GitHub</span>
+                </>
+            ),
+        },
+        {
+            href: 'https://www.linkedin.com/in/cody-miu/',
+            title: 'Visit my Linkedin profile!',
+            label: (
+                <>
+                    <FontAwesomeIcon icon={faLinkedin} className="fa-lg" />
+                    <span>LinkedIn</span>
+                </>
+            ),
         },
     ];
 
     return (
-        <footer
-            id="contact"
-            className="flex flex-col gap-y-4 items-center pt-16 pb-10 text-grey-100"
-        >
-            <h4 className="text-xl font-bold">Get in touch</h4>
-            <p>Montreal, QC, Canada</p>
-            {linkButtons.map((button) => (
-                <Link
-                    key={button.href}
-                    href={button.href}
-                    target="_blank"
-                    rel="noreferrer"
-                    title={button.title}
-                    className="border-b border-transparent hover:border-orange-100"
-                >
-                    {button.label}
-                </Link>
-            ))}
+        <footer id="contact" className="pt-16">
+            <div className="flex flex-col gap-y-4 items-center py-10 text-white bg-grey-120">
+                <h4 className="text-xl font-bold">Get in touch</h4>
+                <div className="flex flex-col gap-y-4">
+                    {linkButtons.map((button) => (
+                        <Link
+                            key={button.href}
+                            href={button.href}
+                            target="_blank"
+                            rel="noreferrer"
+                            title={button.title}
+                            className="w-max space-x-3 hover:text-orange-100"
+                        >
+                            {button.label}
+                        </Link>
+                    ))}
+                    <span className="space-x-3">
+                        <FontAwesomeIcon icon={faLocationPin} className="fa-lg" />
+                        <span>Based in Montreal</span>
+                    </span>
+                </div>
+                <span className="space-x-3">
+                    <FontAwesomeIcon icon={faCopyright} />
+                    <span>2023</span>
+                </span>
+            </div>
         </footer>
     );
 };
