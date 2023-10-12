57c919e05dc13e19a4a7897de17a7972001febbd
Cody06 <codydev22@gmail.com>
2023-10-11 22:20:27 -0400

    Update Logo tailwind to allow animaiton movement

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -6,19 +6,19 @@ const Logo: React.FC<Props> = ({ showAnimation }) => {
     const parallelogramShape =
         'bg-orange-100 w-[6rem] h-[7rem] skew-y-[30deg] rotate-[-60deg] rounded-3xl';
 
-    const topLayer = <div className={`absolute left-9 bg-opacity-80 ${parallelogramShape}`} />;
+    const topLayer = <div className={`relative bg-opacity-80 ${parallelogramShape}`} />;
 
     const middleLayer = (
-        <div className={`absolute top-6 left-9 bg-opacity-60 ${parallelogramShape}`} />
+        <div className={`relative -top-[5rem] bg-opacity-60 ${parallelogramShape}`} />
     );
 
     const bottomLayer = (
-        <div className={`absolute top-12 left-9 bg-opacity-40 ${parallelogramShape}`} />
+        <div className={`relative -top-[10rem] bg-opacity-40 ${parallelogramShape}`} />
     );
 
     return (
         <div
-            className={`relative w-[10.5rem] h-[10rem] ${
+            className={`pl-9 w-[10.5rem] h-[10rem] ${
                 showAnimation && 'animate-slide-rigth-to-left'
             }`}
         >
