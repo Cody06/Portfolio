753353a066a6e361a15debd574eaaccb8b2ad8a5
Cody06 <codydev22@gmail.com>
2023-10-16 23:02:21 -0400

    Create useOutsideClick hook

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+const useOutsideClick = (callback: () => void) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const onOutsideClick = (event: any) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                callback();
+            }
+        };
+
+        document.addEventListener('click', onOutsideClick, true);
+
+        return () => {
+            document.removeEventListener('click', onOutsideClick, true);
+        };
+    }, [ref]);
+
+    return ref;
+};
+
+export default useOutsideClick;
