39d5c2620cb8c72cbad32a58051b95b417e28581
Cody06 <codydev22@gmail.com>
2023-11-07 22:28:03 -0500

    Create initial structure

---
+++ src/app/social/page.tsx
@@ -0,0 +1,11 @@
+import Social from '@/components/Social';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Social',
+    description: 'Social platform to connect with like minded people',
+}
+
+export default function SocialPage() {
+    return <Social />;
+}
