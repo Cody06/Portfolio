209e6e1ac2afbcac40aaef91641418768e5a9050
Cody06 <codydev22@gmail.com>
2023-11-13 21:36:47 -0500

    Create Nav component

---
+++ src/components/Social/components/Nav.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+interface Props {
+    user: string;
+}
+
+const Nav: React.FC<Props> = ({ user }) => {
+    console.log('Nav');
+
+    const buttons = [
+        {
+            id: 'home',
+            name: 'All Posts',
+            onClick: () => {},
+        },
+        {
+            id: 'following',
+            name: 'Following',
+            onClick: () => {},
+        },
+        {
+            id: 'profile',
+            name: `Profile: ${user}`,
+            onClick: () => {},
+        },
+        {
+            id: 'more',
+            name: 'More',
+            onClick: () => {},
+        },
+    ];
+
+    return (
+        <nav className="p-6 rounded-xl shadow-lg w-max">
+            <ul className="flex flex-col gap-y-4">
+                {buttons.map((button) => (
+                    <li
+                        key={button.id}
+                        className="p-2 hover:bg-grey-100 hover:bg-opacity-20 hover:cursor-pointer rounded-lg"
+                    >
+                        <button onClick={button.onClick}>{button.name}</button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
