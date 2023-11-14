ccaad210b8efad8f2d08e48d1597f1a42c2de726
Cody06 <codydev22@gmail.com>
2023-11-13 21:37:14 -0500

    Create initial structure for All Posts section

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
     title: 'Social',
     description: 'Social platform to connect with like minded people',
-}
+};
 
 export default function SocialPage() {
     return <Social />;
--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -1,3 +1,37 @@
-const Social = () => <h1>Social platform is a work in progress</h1>;
+import Link from 'next/link';
+import Nav from './components/Nav';
+
+const Social = () => (
+    <>
+        <div className="flex justify-between p-4 text-white bg-blue-100">
+            <span className="font-bold">Social</span>
+            <Link href="/" className="hover:text-orange-100">
+                Back to Portfolio
+            </Link>
+        </div>
+
+        <div>
+            <Nav user="guest" />
+
+            {/* TODO: Container for the different sections */}
+            <div>
+                <h1>
+                    All Posts
+                </h1>
+
+                <div className="flex flex-col gap-y-4 p-4 border">
+                    <textarea className="border" />
+                    <button className="w-max m-auto border p-2">Post</button>
+                </div>
+
+                <div>
+                    List of posts
+                </div>
+            </div>
+        </div>
+
+
+    </>
+);
 
 export default Social;
