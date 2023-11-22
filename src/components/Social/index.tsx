355ba84cf5b572518a958dbda06f0ef2ff6400a3
Cody06 <codydev22@gmail.com>
2023-11-21 22:46:25 -0500

    Add function to toggle likes

--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -3,24 +3,54 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import Link from 'next/link';
 import Nav from './components/Nav';
+import { useState } from 'react';
 
 const Social = () => {
+    // TODO: Add user Id from login
+    const userId = 'guest';
+
     const initialPosts = [
         {
             id: '1',
             date: new Date('2023-10-12').toDateString(),
             creator: 'cody',
             content: 'Hello and welcome to Social!',
-            likes: 0,
+            likes: ['guest'],
         },
         {
             id: '2',
             date: new Date('2023-10-12').toDateString(),
             creator: 'cody',
             content: 'Looking forward to see what everyone has to share!',
-            likes: 0,
+            likes: [],
         },
     ];
+
+    const [posts, setPosts] = useState(initialPosts);
+
+    const toggleLike = (postId: string) => {
+        setPosts(
+            posts.map((post) => {
+                if (post.id === postId) {
+                    const userAlreadyLikedPost = post.likes.includes(userId);
+
+                    if (userAlreadyLikedPost) {
+                        return {
+                            ...post,
+                            likes: post.likes.filter((likedUserId) => likedUserId !== userId),
+                        };
+                    } else {
+                        return {
+                            ...post,
+                            likes: [...post.likes, userId],
+                        };
+                    }
+                } else {
+                    return post;
+                }
+            }),
+        );
+    };
 
     return (
         <>
@@ -54,7 +84,7 @@ const Social = () => {
                     </div>
 
                     <div className="space-y-4">
-                        {initialPosts.map((post) => (
+                        {posts.map((post) => (
                             <div key={post.id} className="space-y-4 p-4 rounded-xl shadow-md">
                                 <div className="flex justify-between">
                                     <span className="font-bold">{post.creator}</span>
@@ -63,12 +93,10 @@ const Social = () => {
                                 <p>{post.content}</p>
                                 <button
                                     className="text-grey-100 space-x-2 hover:text-red"
-                                    onClick={() =>
-                                        console.log(`Increment post id: ${post.id} by 1`)
-                                    }
+                                    onClick={() => toggleLike(post.id)}
                                 >
                                     <FontAwesomeIcon icon={faHeart} />
-                                    <span>{post.likes}</span>
+                                    <span>{post.likes.length}</span>
                                 </button>
                             </div>
                         ))}
