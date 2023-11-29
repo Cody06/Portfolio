07555ff44c40ccb236dded7db59a16de9276b3f6
Cody06 <codydev22@gmail.com>
2023-11-28 21:22:02 -0500

    Change view from Nav buttons

--- src/components/Social/components/Nav.tsx
+++ src/components/Social/components/Nav.tsx
@@ -1,40 +1,68 @@
+import useOutsideClick from '@/hooks/useOutsideClick';
+import Link from 'next/link';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { Views } from '../types';
+
 interface Props {
     user: string;
+    setSelectedView: Dispatch<SetStateAction<Views>>;
 }
 
-const Nav: React.FC<Props> = ({ user }) => {
+const Nav: React.FC<Props> = ({ user, setSelectedView }) => {
+    console.log('Rendering Nav');
+    const [isOpen, setIsOpen] = useState(false);
+    const ref = useOutsideClick(() => setIsOpen(false));
     const buttons = [
         {
             id: 'home',
             name: 'All Posts',
-            onClick: () => {},
+            onClick: () => setSelectedView('allPosts'),
         },
         {
             id: 'following',
             name: 'Following',
-            onClick: () => {},
+            onClick: () => setSelectedView('following'),
         },
         {
             id: 'profile',
             name: `Profile: ${user}`,
-            onClick: () => {},
+            onClick: () => setSelectedView('ownProfile'),
         },
         {
             id: 'more',
             name: 'More',
-            onClick: () => {},
+            onClick: () => {
+                setIsOpen(!isOpen);
+            },
         },
     ];
+
+    const Dropdown = (
+        <Link
+            className="absolute p-2 w-full bg-white rounded-b-lg shadow-lg
+                hover:bg-grey-90 hover:cursor-pointer"
+            href="/"
+        >
+            Log out
+        </Link>
+    );
 
     return (
         <nav className="p-6 rounded-xl shadow-lg w-max whitespace-nowrap h-max">
             <ul className="flex flex-col gap-y-4">
                 {buttons.map((button) => (
-                    <li
-                        key={button.id}
-                        className="p-2 hover:bg-grey-100 hover:bg-opacity-20 hover:cursor-pointer rounded-lg"
-                    >
-                        <button onClick={button.onClick}>{button.name}</button>
+                    <li ref={ref} key={button.id}>
+                        <button
+                            className={`w-full p-2 text-left rounded-lg 
+                                hover:bg-grey-100 hover:bg-opacity-20 hover:cursor-pointer
+                                ${button.id === 'more' && isOpen && 'bg-grey-100 bg-opacity-20'}`}
+                            onClick={button.onClick}
+                        >
+                            {button.name}
+                        </button>
+                        {button.id === 'more' && (
+                            <div className="relative">{isOpen && Dropdown}</div>
+                        )}
                     </li>
                 ))}
             </ul>
--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -1,69 +1,23 @@
 'use client';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
+
 import { useState } from 'react';
 import Link from 'next/link';
 import Nav from './components/Nav';
-import NewPostInput from './components/NewPostInput';
+import AllPosts from './components/AllPosts';
+import Following from './components/Following';
+import { Views } from './types';
+import OwnProfile from './components/OwnProfile';
 
 const Social = () => {
     // TODO: Add user Id from login
     const userId = 'guest';
 
-    const initialPosts = [
-        {
-            id: '1',
-            date: new Date('2023-10-12').toDateString(),
-            creator: 'cody',
-            content: 'Hello and welcome to Social!',
-            likes: ['guest'],
-        },
-        {
-            id: '2',
-            date: new Date('2023-10-12').toDateString(),
-            creator: 'cody',
-            content: 'Looking forward to see what everyone has to share!',
-            likes: [],
-        },
-    ];
+    const [selectedView, setSelectedView] = useState<Views>('allPosts');
 
-    const [posts, setPosts] = useState(initialPosts);
-
-    const savePost = (newPost: string) => {
-        setPosts([
-            {
-                id: `${userId}-${Date.now().toString()}`,
-                date: new Date().toDateString(),
-                creator: userId,
-                content: newPost,
-                likes: [],
-            },
-            ...posts,
-        ]);
-    };
-
-    const toggleLike = (postId: string) => {
-        setPosts(
-            posts.map((post) => {
-                if (post.id === postId) {
-                    const userAlreadyLikedPost = post.likes.includes(userId);
-
-                    if (userAlreadyLikedPost) {
-                        return {
-                            ...post,
-                            likes: post.likes.filter((likedUserId) => likedUserId !== userId),
-                        };
-                    } else {
-                        return {
-                            ...post,
-                            likes: [...post.likes, userId],
-                        };
-                    }
-                } else {
-                    return post;
-                }
-            }),
-        );
+    const views = {
+        allPosts: <AllPosts userId={userId} />,
+        following: <Following />,
+        ownProfile: <OwnProfile />,
     };
 
     return (
@@ -78,33 +32,9 @@ const Social = () => {
             </div>
 
             <div className="flex p-4 gap-x-4 max-w-[70rem] mx-auto">
-                <Nav user="guest" />
+                <Nav user="guest" setSelectedView={setSelectedView} />
 
-                {/* TODO: Container for the different sections */}
-                <div className="max-w-[100rem] mx-auto">
-                    <h1 className="text-lg text-center mb-4">All Posts</h1>
-
-                    <NewPostInput savePost={savePost} />
-
-                    <div className="space-y-4">
-                        {posts.map((post) => (
-                            <div key={post.id} className="space-y-4 p-4 rounded-xl shadow-md">
-                                <div className="flex justify-between">
-                                    <span className="font-bold">{post.creator}</span>
-                                    <span className="text-grey-100">{post.date}</span>
-                                </div>
-                                <p>{post.content}</p>
-                                <button
-                                    className="text-grey-100 space-x-2 hover:text-red"
-                                    onClick={() => toggleLike(post.id)}
-                                >
-                                    <FontAwesomeIcon icon={faHeart} />
-                                    <span>{post.likes.length}</span>
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                {views[selectedView]}
             </div>
         </>
     );
