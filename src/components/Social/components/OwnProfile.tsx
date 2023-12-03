259acaaca7b114cf4d3b2b4776b421ebbaf37e87
Cody06 <codydev22@gmail.com>
2023-12-03 17:56:50 -0500

    Show posts on user posts on profile view

--- src/components/Social/components/OwnProfile.tsx
+++ src/components/Social/components/OwnProfile.tsx
@@ -1,3 +1,24 @@
-const OwnProfile = () => <div>These are all your posts</div>;
+import { Post } from '../types';
+import PostContainer from './PostContainer';
+
+interface Props {
+    userId: string;
+    userPosts: Post[];
+}
+
+const OwnProfile: React.FC<Props> = ({ userId, userPosts }) => {
+    return (
+        <div className="max-1-[1000rem] mx-auto">
+            <div className="space-x-4">
+                <span>0 Following</span>
+                <span>0 Followers</span>
+            </div>
+            <h1 className="text-lg text-center mb-4">{userId}&apos;s posts</h1>
+            {userPosts.map((post) => (
+                <PostContainer key={post.id} post={post} />
+            ))}
+        </div>
+    );
+};
 
 export default OwnProfile;
