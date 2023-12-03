7d23624a934b91719797cd965017d943676d49c8
Cody06 <codydev22@gmail.com>
2023-12-03 17:55:18 -0500

    Create Post type

--- src/components/Social/types.ts
+++ src/components/Social/types.ts
@@ -1,1 +1,9 @@
 export type Views = 'allPosts' | 'following' | 'ownProfile';
+
+export type Post = {
+    id: string;
+    date: string;
+    creator: string;
+    content: string;
+    likes: string[];
+};
