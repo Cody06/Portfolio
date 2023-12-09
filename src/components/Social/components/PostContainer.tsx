27ccb87831870d5856a26aece46dd010e0ce8a63
Cody06 <codydev22@gmail.com>
2023-12-08 21:51:21 -0500

    PostContainer can select creator profile view

--- src/components/Social/components/PostContainer.tsx
+++ src/components/Social/components/PostContainer.tsx
@@ -4,30 +4,39 @@ import { Post } from '../types';
 
 interface Props {
     post: Post;
-    toggleLike?: (postId: string) => void;
+    loggedUserId: string;
+    toggleLike: (postId: string) => void;
+    showSelectedUserProfile?: (selectedUserId: string) => void;
 }
 
-const PostContainer: React.FC<Props> = ({ post, toggleLike }) => (
+const PostContainer: React.FC<Props> = ({
+    post,
+    loggedUserId,
+    showSelectedUserProfile,
+    toggleLike,
+}) => (
     <div className="space-y-4 p-4 rounded-xl shadow-md">
         <div className="flex justify-between">
-            <span className="font-bold">{post.creator}</span>
+            {post.creator !== loggedUserId && showSelectedUserProfile ? (
+                <button
+                    className="font-bold text-blue-100 hover:text-blue-110"
+                    onClick={() => showSelectedUserProfile(post.creator)}
+                >
+                    {post.creator}
+                </button>
+            ) : (
+                <span className="font-bold">{post.creator}</span>
+            )}
             <span className="text-grey-100">{post.date}</span>
         </div>
         <p>{post.content}</p>
-        {toggleLike ? (
-            <button
-                className="text-grey-100 space-x-2 hover:text-red"
-                onClick={() => toggleLike(post.id)}
-            >
-                <FontAwesomeIcon icon={faHeart} />
-                <span>{post.likes.length}</span>
-            </button>
-        ) : (
-            <div className="text-grey-100 space-x-2">
-                <FontAwesomeIcon icon={faHeart} />
-                <span>{post.likes.length}</span>
-            </div>
-        )}
+        <button
+            className="text-grey-100 space-x-2 hover:text-red"
+            onClick={() => toggleLike(post.id)}
+        >
+            <FontAwesomeIcon icon={faHeart} />
+            <span>{post.likes.length}</span>
+        </button>
     </div>
 );
 
