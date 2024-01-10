6672a1f5165aae38fb98fd14a287ca476cf53a78
Cody06 <codydev22@gmail.com>
2024-01-09 19:55:40 -0500

    Fix typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: 'Cody Miu',
-    description: 'Personal portfolio create with NextJs, React and TypeScript',
+    description: 'Personal portfolio created with NextJs, React and TypeScript',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
