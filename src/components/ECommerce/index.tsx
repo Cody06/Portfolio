5be5f2679af2ed631ce6f00a3316d5ea47acd18e
Cody06 <codydev22@gmail.com>
2024-02-03 22:29:41 -0500

    Add basic nav

--- src/components/ECommerce/index.tsx
+++ src/components/ECommerce/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Image from 'next/image';
+import Link from 'next/link';
 /*
  TODO:
  - Create store-front page
@@ -87,51 +88,62 @@ const ECommerce = () => {
     const viewBookPage = (id: number) => console.log('view book page:', id);
 
     return (
-        <div className="flex flex-wrap gap-4">
-            {books.map((book) => (
-                <article
-                    key={book.id}
-                    className="flex flex-col w-[300px] p-2 items-center border border-grey-90"
-                >
-                    {book.images[0] && (
-                        <button className="bg-grey-100 mb-2" onClick={() => viewBookPage(book.id)}>
-                            <Image
-                                src={`/assets/books/${book.images[0]}`}
-                                width={200}
-                                height={300}
-                                alt="Book cover image"
-                            />
-                        </button>
-                    )}
-
-                    <section className="flex flex-col items-center gap-y-2 mb-2">
-                        <button
-                            className="font-bold hover:text-orange-100"
-                            onClick={() => viewBookPage(book.id)}
-                        >
-                            {book.title}
-                        </button>
-                        <span className="text-grey-110 text-sm">by {book.author}</span>
-                        {/* @TODO: Add reviews section */}
-                        <button
-                            className="text-blue-110"
-                            onClick={() => console.log('view reviews')}
-                        >
-                            {book.rating} / 5
-                        </button>
-                        <p className="font-bold">${book.price}</p>
-                    </section>
-
-                    <button
-                        className="mt-auto px-4 py-2 w-max bg-orange-100 rounded-xl
-                                                hover:brightness-90 active:brightness-75"
-                        onClick={() => console.log('Add to cart:', book.id)}
+        <>
+            <nav className="flex justify-between p-4 text-white bg-blue-110">
+                <span>E-Commerce</span>
+                <Link href="/" className="hover:text-orange-100">
+                    Back to Portfolio
+                </Link>
+            </nav>
+            <div className="flex flex-wrap gap-4">
+                {books.map((book) => (
+                    <article
+                        key={book.id}
+                        className="flex flex-col w-[300px] p-2 items-center border border-grey-90"
                     >
-                        Add to Cart
-                    </button>
-                </article>
-            ))}
-        </div>
+                        {book.images[0] && (
+                            <button
+                                className="bg-grey-100 mb-2"
+                                onClick={() => viewBookPage(book.id)}
+                            >
+                                <Image
+                                    src={`/assets/books/${book.images[0]}`}
+                                    width={200}
+                                    height={300}
+                                    alt="Book cover image"
+                                />
+                            </button>
+                        )}
+
+                        <section className="flex flex-col items-center gap-y-2 mb-2">
+                            <button
+                                className="font-bold hover:text-orange-100"
+                                onClick={() => viewBookPage(book.id)}
+                            >
+                                {book.title}
+                            </button>
+                            <span className="text-grey-110 text-sm">by {book.author}</span>
+                            {/* @TODO: Add reviews section */}
+                            <button
+                                className="text-blue-110"
+                                onClick={() => console.log('view reviews')}
+                            >
+                                {book.rating} / 5
+                            </button>
+                            <p className="font-bold">${book.price}</p>
+                        </section>
+
+                        <button
+                            className="mt-auto px-4 py-2 w-max bg-orange-100 rounded-xl
+                                                hover:brightness-90 active:brightness-75"
+                            onClick={() => console.log('Add to cart:', book.id)}
+                        >
+                            Add to Cart
+                        </button>
+                    </article>
+                ))}
+            </div>
+        </>
     );
 };
 
