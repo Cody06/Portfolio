580fa7a8d774378338494e99d7c126adf5c14e97
Cody06 <codydev22@gmail.com>
2024-02-20 14:44:24 -0500

    Improve Input and add textarea

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,14 +1,26 @@
 interface Props {
     id: string;
     label: string;
+    maxLength: number;
     name: string;
     type: string;
     value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
     error?: string;
+    required?: boolean;
 }
 
-const Input: React.FC<Props> = ({ id, label, name, type, value, onChange, error }) => {
+const Input: React.FC<Props> = ({
+    id,
+    label,
+    maxLength,
+    name,
+    type,
+    value,
+    onChange,
+    error,
+    required,
+}) => {
     /*  Input comes with floating label:
         - label takes the placeholder position when "placeholder is shown" (placeholder is transparent)
         - when placeholder goes away or there's focus, label floats to the top
@@ -18,16 +30,33 @@ const Input: React.FC<Props> = ({ id, label, name, type, value, onChange, error 
         // pt to prevent clipping with other elements
         <div className="pt-3">
             <div className="relative border-2 bg-white border-grey-110 rounded-lg">
-                <input
-                    type={type}
-                    id={id}
-                    name={name}
-                    value={value}
-                    placeholder={label}
-                    onChange={onChange}
-                    className="peer placeholder-transparent w-full px-2 py-3
-                        bg-white rounded-lg focus:outline-none"
-                />
+                {type === 'textarea' ? (
+                    <textarea
+                        id={id}
+                        name={name}
+                        maxLength={maxLength}
+                        value={value}
+                        placeholder={label}
+                        required={required}
+                        onChange={onChange}
+                        className="peer placeholder-transparent w-full px-2 py-3
+                        text-black bg-white rounded-lg focus:outline-none"
+                        rows={2}
+                    />
+                ) : (
+                    <input
+                        type={type}
+                        id={id}
+                        name={name}
+                        value={value}
+                        maxLength={maxLength}
+                        placeholder={label}
+                        required={required}
+                        onChange={onChange}
+                        className="peer placeholder-transparent w-full px-2 py-3
+                            text-black bg-white rounded-lg focus:outline-none"
+                    />
+                )}
                 {/* label needs to be after input for peer selector to work */}
                 <label
                     htmlFor={id}
@@ -35,7 +64,7 @@ const Input: React.FC<Props> = ({ id, label, name, type, value, onChange, error 
                         bg-white rounded-t-md
                         peer-placeholder-shown:top-2 peer-placeholder-shown:text-base
                         peer-focus:-top-3.5 peer-focus:text-sm
-                        transition-all"
+                        hover:cursor-text transition-all"
                 >
                     {label}
                 </label>
