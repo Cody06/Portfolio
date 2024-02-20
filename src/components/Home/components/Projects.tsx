5d9df8097e91593929a06abb9da314ab11af8b7b
Cody06 <codydev22@gmail.com>
2024-02-19 21:39:46 -0500

    Add Carousel to Projects

--- src/components/Home/components/Projects.tsx
+++ src/components/Home/components/Projects.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Button from '../../ui/Button';
 import SectionHeader from './SectionHeader';
+import Carousel from '@/components/ui/Carousel';
 
 interface Props {
     showAnimation: boolean;
@@ -16,7 +17,7 @@ const Projects: React.FC<Props> = ({ showAnimation }) => {
             description: `Workboard is a project managing application. Users can create project boards with different columns
                 and move their tasks between them.`,
             href: '/workboard',
-            thumbnail: 'workboard.png',
+            thumbnail: ['workboard.png', 'workboard-mobile.png'],
         },
         {
             id: 2,
@@ -26,7 +27,7 @@ const Projects: React.FC<Props> = ({ showAnimation }) => {
             description: `This platform allows users to create posts and follow other users. An implementation of the "like"
                 button is also present.`,
             href: '/social',
-            thumbnail: 'social.png',
+            thumbnail: ['social.png', 'social-mobile.png'],
         },
         {
             id: 3,
@@ -36,7 +37,7 @@ const Projects: React.FC<Props> = ({ showAnimation }) => {
             description: `This is an e-commerce platform where users can post items for auction and bid on posted ones. A comment
                 section is also implemented.`,
             href: '',
-            thumbnail: 'commerce.png',
+            thumbnail: ['commerce.png', 'commerce-mobile.png'],
         },
     ];
 
@@ -53,31 +54,72 @@ const Projects: React.FC<Props> = ({ showAnimation }) => {
                             bg-white rounded-lg shadow-lg
                             hover:shadow-xl hover:scale-[1.01] ease-in duration-100"
                     >
-                        <div className="relative max-w-max m-auto">
-                            <Image
-                                src={'/assets/laptop.png'}
-                                width={520}
-                                height={300}
-                                priority
-                                alt="Laptop image"
-                            />
-                            <div className="absolute top-2 w-full h-full px-[10.19%]">
-                                {/* TODO: Remove when this project gets a thumbnail */}
-                                {project.id === 3 ? (
-                                    <div className="text-center pt-[30%] bg-white h-[87%] rounded-t-lg">
-                                        Coming soon
-                                    </div>
-                                ) : (
-                                    <Image
-                                        src={`/assets/thumbnails/${project.thumbnail}`}
-                                        width={414}
-                                        height={267}
-                                        alt={`thumbnail ${project.thumbnail}`}
-                                        className="rounded-t-lg"
-                                    />
-                                )}
-                            </div>
-                        </div>
+                        <Carousel>
+                            {[
+                                {
+                                    item: (
+                                        <div className="relative max-w-max m-auto">
+                                            <Image
+                                                src={'/assets/laptop.png'}
+                                                width={520}
+                                                height={300}
+                                                priority
+                                                alt="Laptop image"
+                                            />
+                                            <div className="absolute top-2 w-full h-full px-[10.19%]">
+                                                {/* TODO: Remove when this project gets a thumbnail */}
+                                                {project.id === 3 ? (
+                                                    <div className="text-center pt-[30%] bg-white h-[87%] rounded-t-lg">
+                                                        Coming soon
+                                                    </div>
+                                                ) : (
+                                                    <Image
+                                                        src={`/assets/thumbnails/${project.thumbnail[0]}`}
+                                                        width={520}
+                                                        height={300}
+                                                        alt="Desktop thumbnail"
+                                                        className="rounded-t-lg"
+                                                    />
+                                                )}
+                                            </div>
+                                        </div>
+                                    ),
+                                    title: `${project.name}-desktop`,
+                                },
+                                {
+                                    item: (
+                                        <div className="relative max-w-max max-h-[300px] m-auto">
+                                            <Image
+                                                src={'/assets/phone.png'}
+                                                width={150}
+                                                height={300}
+                                                alt="Phone image"
+                                            />
+                                            {project.id === 3 ? (
+                                                <div
+                                                    className="absolute top-[0.4rem] w-[140px] h-[280px] pt-[50%]
+                                                        left-[50%] -translate-x-[50%]
+                                                        text-center bg-white rounded-lg"
+                                                >
+                                                    Coming soon
+                                                </div>
+                                            ) : (
+                                                <Image
+                                                    src={`/assets/thumbnails/${project.thumbnail[1]}`}
+                                                    width={140}
+                                                    height={290}
+                                                    alt="Mobile thumbnail"
+                                                    className="absolute top-[0.4rem]
+                                                        left-[50%] -translate-x-[50%]
+                                                        border-y-4 border-black rounded-lg"
+                                                />
+                                            )}
+                                        </div>
+                                    ),
+                                    title: `${project.name}-mobile`,
+                                },
+                            ]}
+                        </Carousel>
 
                         <div className="pt-4 md:pt-0 md:w-2/3">
                             <h3 className="mb-4 text-2xl text-center font-medium">
