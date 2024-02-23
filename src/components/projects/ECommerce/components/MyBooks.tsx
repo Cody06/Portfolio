057a1f01fb072cd0e477e4791d39fd51ab09a48f
Cody06 <codydev22@gmail.com>
2024-02-22 22:05:01 -0500

    Create MyBooks layout

---
+++ src/components/projects/ECommerce/components/MyBooks.tsx
@@ -0,0 +1,10 @@
+import SectionHeader from './SectionHeader';
+
+export default function MyBooks() {
+    return (
+        <section className="mx-4">
+            <SectionHeader title="My books collection" />
+            {/* TODO: Pass a list of books */}
+        </section>
+    );
+}
