5a7eae2e790217250f11762df02a3fad220c5ad3
Cody06 <codydev22@gmail.com>
2024-03-01 22:20:01 -0500

    Create DeletePostModal

---
+++ src/components/projects/Social/DeletePostModal.tsx
@@ -0,0 +1,35 @@
+import Modal from '@/components/Modal';
+import ModalBody from '@/components/Modal/ModalBody';
+import useStore from './Store';
+
+export default function DeletePostModal() {
+    const { isDeletePostModalOpen, deletePost, setIsDeletePostModalOpen } = useStore();
+    const requestClose = () => setIsDeletePostModalOpen(false);
+
+    return (
+        <Modal isOpen={isDeletePostModalOpen} title="Delete post" requestClose={requestClose}>
+            <ModalBody>
+                <h1 className="mb-5">Are you sure you want to delete your post?</h1>
+                <div className="flex gap-x-4">
+                    <button
+                        className="w-full p-1 text-white bg-red border border-red rounded-md
+                            hover:brightness-90 active:brightness-75"
+                        onClick={() => {
+                            deletePost();
+                            requestClose();
+                        }}
+                    >
+                        Yes, delete
+                    </button>
+                    <button
+                        className="w-full p-1 text-grey-120 border border-grey-120 rounded-md
+                            hover:bg-grey-100 hover:bg-opacity-20"
+                        onClick={requestClose}
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </ModalBody>
+        </Modal>
+    );
+}
