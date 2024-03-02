d74d14bba17f277314e6fab39e202b0607911405
Cody06 <codydev22@gmail.com>
2024-03-02 17:35:58 -0500

    Update Nav

--- src/components/projects/Social/Nav.tsx
+++ src/components/projects/Social/Nav.tsx
@@ -1,38 +1,48 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faUser, faUserGroup } from '@fortawesome/free-solid-svg-icons';
+
 import Link from 'next/link';
 import { loggedUserId } from './data';
+import NavDropdown from './NavDropdown';
 
 export default function Nav() {
     const navLinks = [
         {
             id: 'home',
+            icon: <FontAwesomeIcon icon={faHouse} className="w-4" />,
             name: 'All Posts',
             href: '/social',
         },
         {
             id: 'profile',
+            icon: <FontAwesomeIcon icon={faUser} className="w-4" />,
             name: `Profile: ${loggedUserId}`,
             href: `/social/profile/${loggedUserId}`,
         },
         {
             id: 'following',
+            icon: <FontAwesomeIcon icon={faUserGroup} className="w-4" />,
             name: 'Following',
             href: '/social/following',
         },
     ];
+
     return (
         <nav className="p-3 md:p-6 w-max h-max mx-auto md:mx-0 mb-6 rounded-xl shadow-lg whitespace-nowrap">
-            <ul className="flex flex-row md:flex-col gap-y-3">
-                {navLinks.map((link) => (
-                    <li key={link.id}>
+            <ul className="flex flex-row md:flex-col space-y-2">
+                {navLinks.map(({ id, icon, name, href }) => (
+                    <li key={id}>
                         <Link
-                            className="w-full p-2 text-left rounded-lg 
-                                hover:bg-grey-100 hover:bg-opacity-20 hover:cursor-pointer"
-                            href={link.href}
+                            className="block text-left p-1 rounded-lg space-x-3
+                                hover:bg-grey-100 hover:cursor-pointer"
+                            href={href}
                         >
-                            {link.name}
+                            <span className="text-grey-110">{icon}</span>
+                            <span>{name}</span>
                         </Link>
                     </li>
                 ))}
+                <NavDropdown />
             </ul>
         </nav>
     );
