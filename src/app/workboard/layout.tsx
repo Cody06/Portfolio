d9ce9fc839814e6a8ff35146aa7ae2b6e8763ae5
Cody06 <codydev22@gmail.com>
2024-03-05 17:32:35 -0500

    Minor update to workboard layout

--- src/app/workboard/layout.tsx
+++ src/app/workboard/layout.tsx
@@ -13,7 +13,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <body className="bg-blue-100">
             <nav className="flex justify-between mb-8 bg-blue-110 p-4 text-white">
-                <span className="font-bold">Workboard</span>
+                <Link className="font-bold" href="/workboard">
+                    Workboard
+                </Link>
                 <Link href="/" className="hover:text-orange-100" scroll={false}>
                     Back to Portfolio
                 </Link>
