a64cbe8a9efa13aa668fe95ff1af6c9c3e0da95f
Cody06 <codydev22@gmail.com>
2024-03-29 15:50:42 -0400

    Refactor usePageBottom

--- src/hooks/usePageBottom.ts
+++ src/hooks/usePageBottom.ts
@@ -1,24 +1,21 @@
 import { useEffect, useState } from 'react';
 
-const usePageBottom = () => {
+const usePageBottom = (distanceFromPageBottom: number) => {
     const [reachedBottom, setReachedBottom] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
-            const offsetHeight = document.documentElement.offsetHeight;
-            const innerHeight = window.innerHeight;
-            const scrollTop = document.documentElement.scrollTop;
-            const distanceFromBottomOfPage = 300; // in pixels
+            const documentHeight = document.documentElement.offsetHeight;
+            const windowHeight = window.innerHeight;
+            const pixelsScrolled = document.documentElement.scrollTop;
             const hasReachedBottom =
-                offsetHeight - (innerHeight + scrollTop) <= distanceFromBottomOfPage;
-
+                documentHeight - (windowHeight + pixelsScrolled) <= distanceFromPageBottom;
             setReachedBottom(hasReachedBottom);
         };
 
         window.addEventListener('scroll', handleScroll);
-
         return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+    }, [distanceFromPageBottom]);
 
     return reachedBottom;
 };
