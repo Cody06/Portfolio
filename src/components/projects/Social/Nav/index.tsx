6de998b6fbfba7d4041006cd2a2edb2a50cfa28e
Cody06 <codydev22@gmail.com>
2024-03-28 22:13:18 -0400

    Refactor Social Nav

--- src/components/projects/Social/Nav/index.tsx
+++ src/components/projects/Social/Nav/index.tsx
@@ -3,20 +3,21 @@ import { faHouse, faUser, faUserGroup } from '@fortawesome/free-solid-svg-icons'
 
 import Link from 'next/link';
 import { loggedUserId } from '../data';
-import NavDropdown from './NavDropdown';
+import Dropdown from './Dropdown';
+import { iconBgHover } from '@/components/ui/tailwindStyles';
 
 export default function Nav() {
     const navLinks = [
         {
             id: 'home',
             icon: <FontAwesomeIcon icon={faHouse} className="w-4" />,
-            name: 'All Posts',
+            name: 'Home',
             href: '/social',
         },
         {
             id: 'profile',
             icon: <FontAwesomeIcon icon={faUser} className="w-4" />,
-            name: `Profile: ${loggedUserId}`,
+            name: 'Profile',
             href: `/social/profile/${loggedUserId}`,
         },
         {
@@ -28,25 +29,22 @@ export default function Nav() {
     ];
 
     const centerElement = 'left-1/2 -translate-x-1/2 md:left-auto md:translate-x-0';
+    const commonStyle = `rounded-full px-4 py-1 space-x-3 ${iconBgHover}`;
 
     return (
         <nav
-            className={`fixed z-10 top-16 ${centerElement} md:p-6 w-max h-max rounded-xl shadow-md bg-white p-3 whitespace-nowrap`}
+            className={`fixed z-10 top-16 ${centerElement} md:p-4 w-max h-max rounded-xl shadow-md bg-white p-3 whitespace-nowrap`}
         >
-            <ul className="flex flex-row md:flex-col gap-x-4 gap-y-2">
+            <ul className="flex flex-row md:flex-col gap-x-4 gap-y-2 font-medium">
                 {navLinks.map(({ id, icon, name, href }) => (
                     <li key={id}>
-                        <Link
-                            className="block text-left p-1 rounded-lg space-x-3
-                                hover:bg-neutral-500 hover:cursor-pointer"
-                            href={href}
-                        >
+                        <Link href={href} className={`block ${commonStyle}`}>
                             <span className="text-neutral-500">{icon}</span>
                             <span className="hidden md:inline">{name}</span>
                         </Link>
                     </li>
                 ))}
-                <NavDropdown />
+                <Dropdown style={commonStyle} />
             </ul>
         </nav>
     );
