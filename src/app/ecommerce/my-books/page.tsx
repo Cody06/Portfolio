ca7499c422bccdf648a46b2ce4286f185527d2bf
Cody06 <codydev22@gmail.com>
2024-04-01 12:07:16 -0400

    Update my-books page UI

--- src/app/ecommerce/my-books/page.tsx
+++ src/app/ecommerce/my-books/page.tsx
@@ -1,40 +1,42 @@
 'use client';
 import SectionHeader from '@/components/projects/Ecommerce/SectionHeader';
 import useStore from '@/components/projects/Ecommerce/Store';
+import SecondaryButton from '@/components/projects/Ecommerce/ui/SecondaryButton';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Page() {
     const { myBooks } = useStore();
     return (
-        <main className="w-max mx-auto">
+        <main className="max-w-fit mx-auto">
             <SectionHeader title="My books" />
             {myBooks.length > 0 ? (
-                <section className="flex flex-col gap-y-4">
+                <section className="flex flex-wrap justify-center gap-4">
                     {myBooks.map((book) => (
                         <article
                             key={book.id}
-                            className="flex flex-col items-center gap-y-4 border border-neutral-200 rounded-md p-4"
+                            className="flex flex-col items-center w-[18.75rem] gap-y-4
+                                border border-neutral-200 rounded-lg p-4"
                         >
-                            <h2 className="font-bold">{book.title}</h2>
                             <Image
                                 src={`/assets/books/${book.images[0]}`}
                                 width={200}
                                 height={300}
                                 alt={`Cover ${book.title}`}
                             />
-                            <button className="border rounded-md px-4 py-2">Read</button>
+                            <h2 className="font-bold">{book.title}</h2>
+                            <section className="mt-auto">
+                                <SecondaryButton label="Read" />
+                            </section>
                         </article>
                     ))}
                 </section>
             ) : (
-                <div>
+                <section className="flex flex-col gap-y-4">
                     <h2>You don&apos;t have any books...</h2>
                     <p>Go to store to browse the selection</p>
-                    <Link href="/ecommerce" className="font-bold underline">
-                        Store
-                    </Link>
-                </div>
+                    <SecondaryButton label="Store" href="/ecommerce" />
+                </section>
             )}
         </main>
     );
