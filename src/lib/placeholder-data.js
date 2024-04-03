77489de76b2d754ac5265dddd3caa5110bbf5451
Cody06 <codydev22@gmail.com>
2024-04-03 16:07:30 -0400

    Create placeholder-data

---
+++ src/lib/placeholder-data.js
@@ -0,0 +1,14 @@
+/*
+    This file contains placeholder data that can be used to seed the DB
+*/
+
+const users = [
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a6442a',
+        name: 'Guest',
+        email: 'guest@email.com',
+        password: '123456',
+    },
+];
+
+export { users };
