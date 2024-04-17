865a2a9287868536fc6369a0c727ba44de64105c
Cody06 <codydev22@gmail.com>
2024-04-16 21:07:23 -0400

    Use URL params to handle search

--- src/components/projects/Ecommerce/StoreView.tsx
+++ src/components/projects/Ecommerce/StoreView.tsx
@@ -8,40 +8,45 @@ import { useState } from 'react';
 import SearchInput from './components/SearchInput';
 import SortInput from './components/SortInput';
 import SecondaryButton from './ui/SecondaryButton';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { Book } from '@/app/lib/types';
 
-export default function StoreView() {
-    const { storeItems, myBooks } = useStore();
+export default function StoreView({ items }: { items: Book[] }) {
+    const { myBooks } = useStore();
     const ownedBooksIds = myBooks.map((myBook) => myBook.id);
-
     const [selectedSortOrder, setSelectedSortOrder] = useState('relevance');
-    const [filteredItems, setFilteredItems] = useState(storeItems);
-    let sortedItems = filteredItems;
+
+    let sortedItems = items;
 
     switch (selectedSortOrder) {
-        case 'relevance':
-            sortedItems = filteredItems.sort((a, b) => Number(a.id) - Number(b.id));
-            break;
         case 'priceDesc':
-            sortedItems = filteredItems.sort((a, b) => b.price - a.price);
+            sortedItems = items.sort((a, b) => b.price - a.price);
             break;
         case 'priceAsc':
-            sortedItems = filteredItems.sort((a, b) => a.price - b.price);
+            sortedItems = items.sort((a, b) => a.price - b.price);
             break;
         case 'ratingDesc':
-            sortedItems = filteredItems.sort((a, b) => b.rating - a.rating);
+            sortedItems = items.sort((a, b) => b.rating - a.rating);
             break;
         case 'newest':
-            sortedItems = filteredItems.sort((a, b) => b.publicationDate - a.publicationDate);
+            sortedItems = items.sort((a, b) => b.publicationYear - a.publicationYear);
         default:
+            // Relevance order is default
             break;
     }
 
-    const handleSubmit = (input: string) => {
-        setFilteredItems(
-            storeItems.filter((item) => {
-                return item.title.toLowerCase().includes(input.toLowerCase());
-            }),
-        );
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    const handleSearch = (term: string) => {
+        const params = new URLSearchParams(searchParams);
+        if (term) {
+            params.set('query', term);
+        } else {
+            params.delete('query');
+        }
+        replace(`${pathname}?${params.toString()}`);
     };
 
     return (
@@ -52,46 +57,53 @@ export default function StoreView() {
                     selectedSortOrder={selectedSortOrder}
                     setSelectedSortOrder={setSelectedSortOrder}
                 />
-                <SearchInput onSubmit={handleSubmit} />
+                <SearchInput
+                    defaultValue={searchParams.get('query')?.toString()}
+                    onSubmit={handleSearch}
+                />
             </div>
-            <div className="flex flex-wrap justify-center gap-4">
-                {sortedItems.map((item) => (
-                    <article
-                        key={item.id}
-                        className="flex flex-col w-[18.75rem] p-4 items-center border border-neutral-200 rounded-lg"
-                    >
-                        {item.images[0] && (
-                            <Link href={`/ecommerce/item/${item.id}`} className="mb-2">
-                                <Image
-                                    src={`/assets/books/${item.images[0]}`}
-                                    width={200}
-                                    height={300}
-                                    alt="Book cover image"
-                                />
-                            </Link>
-                        )}
-                        <section className="flex flex-col items-center gap-y-2 mb-2">
-                            <Link
-                                href={`/ecommerce/item/${item.id}`}
-                                className="font-bold hover:text-amber-500"
-                            >
-                                {item.title}
-                            </Link>
-                            <span className="text-neutral-500 text-sm">by {item.author}</span>
-                            <span className="font-bold text-sky-900">{item.rating} / 5</span>
-                            <span className="font-bold">${item.price}</span>
-                        </section>
-
-                        <section className="mt-auto">
-                            {ownedBooksIds.includes(item.id) ? (
-                                <SecondaryButton label="Owned" href="/ecommerce/my-books" />
-                            ) : (
-                                <AddToCartButton item={item} />
+            {sortedItems.length > 0 ? (
+                <div className="flex flex-wrap justify-center gap-4">
+                    {items.map((item) => (
+                        <article
+                            key={item.id}
+                            className="flex flex-col w-[18.75rem] p-4 items-center border border-neutral-200 rounded-lg"
+                        >
+                            {item.image && (
+                                <Link href={`/ecommerce/item/${item.id}`} className="mb-2">
+                                    <Image
+                                        src={`/assets/books/${item.image}`}
+                                        width={200}
+                                        height={300}
+                                        alt="Book cover image"
+                                    />
+                                </Link>
                             )}
-                        </section>
-                    </article>
-                ))}
-            </div>
+                            <section className="flex flex-col items-center gap-y-2 mb-2">
+                                <Link
+                                    href={`/ecommerce/item/${item.id}`}
+                                    className="font-bold hover:text-amber-500"
+                                >
+                                    {item.title}
+                                </Link>
+                                <span className="text-neutral-500 text-sm">by {item.author}</span>
+                                <span className="font-bold text-sky-900">{item.rating} / 5</span>
+                                <span className="font-bold">${item.price}</span>
+                            </section>
+
+                            <section className="mt-auto">
+                                {ownedBooksIds.includes(item.id) ? (
+                                    <SecondaryButton label="Owned" href="/ecommerce/my-books" />
+                                ) : (
+                                    <AddToCartButton item={item} />
+                                )}
+                            </section>
+                        </article>
+                    ))}
+                </div>
+            ) : (
+                <h2>There are no books matching your search</h2>
+            )}
         </section>
     );
 }
