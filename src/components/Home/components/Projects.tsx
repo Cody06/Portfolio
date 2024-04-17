f7da088c6944105e01fcacf323e6899e603cd07e
Cody06 <codydev22@gmail.com>
2024-04-17 13:36:11 -0400

    Increase project images size

--- src/components/Home/components/Projects.tsx
+++ src/components/Home/components/Projects.tsx
@@ -17,7 +17,7 @@ export default function Projects({ showAnimation }: Props) {
             description: `Workboard is a project managing application. Users can create project boards with different columns
                 and move their tasks between them.`,
             href: '/workboard',
-            thumbnail: ['workboard.png', 'workboard-mobile.png'],
+            thumbnails: ['workboard.png', 'workboard-mobile.png'],
         },
         {
             id: 2,
@@ -26,16 +26,15 @@ export default function Projects({ showAnimation }: Props) {
             description: `This platform allows users to create posts and follow other users. An implementation of the "like"
                 button is also present.`,
             href: '/social',
-            thumbnail: ['social.png', 'social-mobile.png'],
+            thumbnails: ['social.png', 'social-mobile.png'],
         },
         {
             id: 3,
             name: 'E-Commerce',
             tools: ['React', 'TypeScript'],
-            description: `This is an e-commerce platform where users can post items for auction and bid on posted ones. A comment
-                section is also implemented.`,
+            description: `This is an e-commerce platform where users can filter and sort books. A checkout flow is also implemented.`,
             href: '/ecommerce',
-            thumbnail: ['commerce.png', 'commerce-mobile.png'],
+            thumbnails: ['ecommerce.png', 'commerce-mobile.png'],
         },
     ];
 
@@ -48,72 +47,65 @@ export default function Projects({ showAnimation }: Props) {
                 {projects.map((project) => (
                     <Card key={project.id}>
                         <div className="flex flex-col md:flex-row md:gap-x-8 lg:gap-x-16">
-                            <Carousel>
-                                {[
-                                    {
-                                        item: (
-                                            <div className="relative max-w-max m-auto">
-                                                <Image
-                                                    src={'/assets/laptop.png'}
-                                                    width={520}
-                                                    height={300}
-                                                    priority
-                                                    alt="Laptop image"
-                                                />
-                                                <div className="absolute top-2 w-full h-full px-[10.19%]">
-                                                    {/* TODO: Remove when this project gets a thumbnail */}
-                                                    {project.id === 3 ? (
-                                                        <div className="text-center pt-[30%] bg-white h-[87%] rounded-t-lg">
-                                                            Coming soon
-                                                        </div>
-                                                    ) : (
+                            <section className="md:w-1/2">
+                                <Carousel>
+                                    {[
+                                        {
+                                            item: (
+                                                <div className="relative">
+                                                    <Image
+                                                        src={'/assets/laptop.png'}
+                                                        width={520}
+                                                        height={300}
+                                                        priority
+                                                        alt="Generic laptop"
+                                                        style={{
+                                                            width: '100%',
+                                                        }}
+                                                    />
+                                                    <div className="absolute top-2 px-[10%] pb-4 h-max">
                                                         <Image
-                                                            src={`/assets/thumbnails/${project.thumbnail[0]}`}
+                                                            src={`/assets/thumbnails/${project.thumbnails[0]}`}
                                                             width={520}
                                                             height={300}
                                                             alt="Desktop thumbnail"
-                                                            className="rounded-t-lg"
+                                                            className="rounded-t-xl"
+                                                            style={{
+                                                                width: '100%',
+                                                                height: '100%',
+                                                            }}
                                                         />
-                                                    )}
-                                                </div>
-                                            </div>
-                                        ),
-                                        title: `${project.name}-desktop`,
-                                    },
-                                    {
-                                        item: (
-                                            <div className="relative max-w-max max-h-[300px] m-auto">
-                                                <Image
-                                                    src={'/assets/phone.png'}
-                                                    width={150}
-                                                    height={300}
-                                                    alt="Phone image"
-                                                />
-                                                {project.id === 3 ? (
-                                                    <div
-                                                        className="absolute top-[0.4rem] w-[140px] h-[280px] pt-[50%]
-                                                        center-element text-center bg-white rounded-lg"
-                                                    >
-                                                        Coming soon
                                                     </div>
-                                                ) : (
-                                                    <Image
-                                                        src={`/assets/thumbnails/${project.thumbnail[1]}`}
-                                                        width={140}
-                                                        height={290}
-                                                        alt="Mobile thumbnail"
-                                                        className="absolute top-[0.4rem] center-element
-                                                        border-y-4 border-black rounded-lg"
-                                                    />
-                                                )}
-                                            </div>
-                                        ),
-                                        title: `${project.name}-mobile`,
-                                    },
-                                ]}
-                            </Carousel>
+                                                </div>
+                                            ),
+                                            title: `${project.name}-desktop`,
+                                        },
+                                        // {
+                                        //     item: (
+                                        //         <div className="relative max-w-max max-h-[300px] m-auto">
+                                        //             <Image
+                                        //                 src={'/assets/phone.png'}
+                                        //                 width={150}
+                                        //                 height={300}
+                                        //                 alt="Phone image"
+                                        //             />
+                                        //             <Image
+                                        //                 src={`/assets/thumbnails/${project.thumbnails[1]}`}
+                                        //                 width={140}
+                                        //                 height={290}
+                                        //                 alt="Mobile thumbnail"
+                                        //                 className="absolute top-[0.4rem] center-element
+                                        //                     border-y-4 border-black rounded-lg"
+                                        //             />
+                                        //         </div>
+                                        //     ),
+                                        //     title: `${project.name}-mobile`,
+                                        // },
+                                    ]}
+                                </Carousel>
+                            </section>
 
-                            <div className="pt-4 md:pt-0 md:w-2/3">
+                            <section className="pt-4 md:pt-0 md:w-1/2">
                                 <h3 className="mb-4 text-2xl text-center font-medium">
                                     {project.name}
                                 </h3>
@@ -139,7 +131,7 @@ export default function Projects({ showAnimation }: Props) {
                                         shadow
                                     />
                                 </div>
-                            </div>
+                            </section>
                         </div>
                     </Card>
                 ))}
